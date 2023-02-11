c6e316c39d3f6e1e120ca2c91719ffcce2792549
alappu900 <122367851+alappu900@users.noreply.github.com>
2023-02-12 00:48:57 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,9 +3,9 @@ class script(object):
 
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/filesharebotusers>𝙰𝙼𝙻 𝚄𝙿𝙳𝙰𝚃𝙴𝚂</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : 𝙼𝙾𝙽𝙶𝙾-𝙳𝙱
+✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://docs.pyrogram.org/>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org/download/releases/3.0/>𝙿𝚈𝚃𝙷𝙾𝙽 𝟹</a>
+✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://www.mongodb.com/>𝙼𝙾𝙽𝙶𝙾-𝙳𝙱</a>
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁 : <a href=https://heroku.com/>𝙷𝙴𝚁𝙾𝙺𝚄</a>
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚅𝙴𝚁𝚂𝙸𝙾𝙽: 𝚅𝙸𝙹𝙰𝚈𝚃𝙷𝙰𝙻𝙰𝙿𝙰𝚃𝙷𝚈 𝙱𝙾𝚃 𝚟3.0.0"""
 
